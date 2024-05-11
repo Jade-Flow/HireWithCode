ad4102915e12737be6aa10e72026e1cd4f52ccfe
shellweta1k <shellwe7alk@gmail.com>
2024-05-11 19:25:27 +0800

    finished

--- public/server/db/index.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-const supabase = createClient(
-  "https://wjjsqstugoqcdmatgxqr.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqanNxc3R1Z29xY2RtYXRneHFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzNTY2MDMsImV4cCI6MjAzMDkzMjYwM30.HH3Mx8gJiDonlwwGhuTHZFvxvVIrObRPE5DAbbEEeW4"
-);
-
-module.exports = supabase;
